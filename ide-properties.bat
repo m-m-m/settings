633c87aa8a93a677221721198e9d5674625d244b
Joerg Hohwiller <hohwille@users.sourceforge.net>
2015-05-02 10:51:08 +0200

    m-m-m/mmm#143: added settings

---
+++ ide-properties.bat
@@ -0,0 +1,6 @@
+@echo off
+rem ****************************************
+rem	Architect
+
+rem Variable replacement config
+set REPLACEMENT_PATTERNS_PATH=workspaces\trunk\settings\eclipse\workspace\replacement-patterns.properties
