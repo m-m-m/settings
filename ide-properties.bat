541727336c54d8463be77d3b29adb66fa8752bca
Joerg Hohwiller <hohwille@users.sourceforge.net>
2016-08-06 11:07:58 +0200

    improved and modernized code style

--- ide-properties.bat
+++ ide-properties.bat
@@ -3,4 +3,4 @@ rem ****************************************
 rem	Architect
 
 rem Variable replacement config
-set REPLACEMENT_PATTERNS_PATH=workspaces\trunk\settings\eclipse\workspace\replacement-patterns.properties
+set REPLACEMENT_PATTERNS_PATH=workspaces\main\settings\eclipse\workspace\replacement-patterns.properties
