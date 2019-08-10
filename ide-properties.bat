02c6364dc1ab37af3aadfd2c8cc3157825d836d9
Jörg Hohwiller <hohwille@users.sourceforge.net>
2019-08-10 21:34:26 +0200

    updated settings

--- ide-properties.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-rem ****************************************
-rem	Architect
-
-rem Variable replacement config
-set REPLACEMENT_PATTERNS_PATH=workspaces\main\settings\eclipse\workspace\replacement-patterns.properties
